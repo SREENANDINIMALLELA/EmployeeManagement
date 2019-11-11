1010d7ebfaa2e78cfcc68f534656c17a59b44a6e
sreenandini <mallelasreenandini2@gmail.com>
2019-11-11 19:16:02 +0000

    initial commit

--- Employee/src/main/java/management/Manager.java
+++ Employee/src/main/java/management/Manager.java
@@ -1,4 +1,15 @@
 package management;
 
-public class Manager {
+import staff.Employee;
+
+public class Manager extends Employee {
+    private String depName;
+    public Manager(String name ,String nI , int salary,String depName){
+        super(name ,nI,salary);
+        this.depName=depName;
+    }
+
+    public String getDepName() {
+        return depName;
+    }
 }
---
+++ Employee/src/test/java/StaffTest/EmployeeTest.java
@@ -0,0 +1,4 @@
+package StaffTest;
+
+public class EmployeeTest {
+}
